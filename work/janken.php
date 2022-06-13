b25ea7a71e8fe1f756c6b44de323d8c85db47e44
cocoared <dmm23program@gmail.com>
2022-06-13 21:59:58 +0900

    [Fix]じゃんけんゲーム開始画面

--- work/janken.php
+++ work/janken.php
@@ -1,11 +1,19 @@
-<?php
-$title = 'じゃんけんゲーム-';
-include '_header.php';
-?>
-  <section>
-    <img src="img/janken.png" width="400" height="260" alt="じゃんけんゲームの紹介画像">
-    <h1>じゃんけんゲーム</h1>
-    <p></p>
-    <a href="index.php">もどる</a>
-  </section>
-<?php include '_footer.php';
+<head>
+  <meta chara-set="UTF-8">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <title>じゃんけんゲーム</title>
+</head>
+<div class="container">
+      <div class="d-flex justify-content-center">
+        <img src="img/janken.png" width="400" />
+      </div>
+      <form class="d-flex justify-content-center" method="post" action="game.php">
+        <div class="mt-3 col-5">
+          <label for="user_name" class="form-label">ユーザー名</label>
+          <input type="text" class="form-control" id="user_name" name="user_name">
+          <div class="d-flex justify-content-center">
+            <button type="submit" class="btn btn-primary mt-3 pl-3 pr-3">始める</button>
+          </div>
+        </div>
+      </form>
+</div>
