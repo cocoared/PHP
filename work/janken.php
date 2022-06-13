165d200ca6bda1d9d53bc0d3886246a2dbefa598
cocoared <dmm23program@gmail.com>
2022-06-13 21:38:11 +0900

    [Create]じゃんけんゲーム開始画面

---
+++ work/janken.php
@@ -0,0 +1,11 @@
+<?php
+$title = 'じゃんけんゲーム-';
+include '_header.php';
+?>
+  <section>
+    <img src="img/janken.png" width="400" height="260" alt="じゃんけんゲームの紹介画像">
+    <h1>じゃんけんゲーム</h1>
+    <p></p>
+    <a href="index.php">もどる</a>
+  </section>
+<?php include '_footer.php';
