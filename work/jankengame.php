394ea002a92a5ca3cd20c5827496526e3a54baa1
cocoared <dmm23program@gmail.com>
2022-06-17 15:18:43 +0900

    [Add]janken_choki

--- work/jankengame.php
+++ work/jankengame.php
@@ -9,5 +9,9 @@
     <div class="text-danger">xxxさんの勝ち</div>
     <div class="text-success">n回のうち、n回勝ちました！</div>
   </div>
-  
+<div class=d-flex justify-content-center align-items-center flex-column mt-2">
+  <div class="border rounded p-3 mt-2">
+    <img src="img/janken_choki.png" width="100" class="rounded" />
+</div>
+</div>
 </div>
